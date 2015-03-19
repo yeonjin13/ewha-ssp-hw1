0a77e2893c21263ab7d33ff2a2c09168f1765937
Yeonjin13 <yakkuyj@gmail.com>
2015-03-19 22:19:20 +0900

    test2

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -5,13 +5,15 @@
  * GitHub Setup Homework 
  * Instructor: Prof. HyungJune Lee
  *
- * Coded by Firstname Lastname
+ * Coded by YeonJin Jin
  * Mar xx, 2015
  *
  */
 
 // Add #define pre-processor below
 
+
+#include "Lab1_1.h"
  
 void setup() {
   //Add your code of Lab Assignment #1
