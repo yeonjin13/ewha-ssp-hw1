36f5f91531677c232eeea7ba1b4aaf431c4d003a
Yeonjin13 <yakkuyj@gmail.com>
2015-03-20 12:43:13 +0900

    Clear the code! :D

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -6,7 +6,7 @@
  * Instructor: Prof. HyungJune Lee
  *
  * Coded by YeonJin Jin
- * Mar xx, 2015
+ * Mar 20, 2015
  *
  */
 
@@ -14,13 +14,23 @@
 
 
 #include "Lab1_1.h"
+
+int FRONT_LED = 10;
+int REAR_LED = 9;
  
 void setup() {
   //Add your code of Lab Assignment #1
-
+  pinMode(FRONT_LED, OUTPUT);
+  pinMode(REAR_LED, OUTPUT);
 }
 
 void loop() {
   //Add your code of Lab Assignment #1  
+  digitalWrite(FRONT_LED, HIGH);
+  digitalWrite(REAR_LED, LOW);
+  delay(1000);
 
+  digitalWrite(FRONT_LED, LOW);
+  digitalWrite(REAR_LED, HIGH);
+  delay(1000);
 }
